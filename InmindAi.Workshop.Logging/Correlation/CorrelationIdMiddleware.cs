c87d328be4cbd3edfa63bf21723dcac9fe733ab2
Elie Khadij <elie.khadij@inmind.ai>
2025-02-25 17:33:28 +0200

    chore: Add HealthChecks, Enrichers, CorrelationId implementation

---
+++ InmindAi.Workshop.Logging/Correlation/CorrelationIdMiddleware.cs
@@ -0,0 +1,48 @@
+﻿
+namespace InmindAi.Workshop.Logging.Correlation;
+
+public class CorrelationIdMiddleware
+{
+    private const string _correlationIdHeader = "X-Correlation-Id";
+    private readonly RequestDelegate _next;
+    public CorrelationIdMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var correlationIdGenerator = context.RequestServices.GetRequiredService<ICorrelationIdGenerator>();
+        var correlationId = GetCorrelationIdTrace(context, correlationIdGenerator);
+        AddCorrelationId(context, correlationId);
+        await _next(context);
+    }
+
+    #region Private Methods
+
+    private static string GetCorrelationIdTrace(HttpContext context, ICorrelationIdGenerator correlationIdGenerator)
+    {
+        var correlation = context.Request.Headers[_correlationIdHeader].FirstOrDefault();
+        if (!string.IsNullOrEmpty(correlation))
+        {
+            correlationIdGenerator.Set(correlation);
+            return correlation;
+        }
+        else
+        {
+            return correlationIdGenerator.Get();
+        }
+    }
+    private void AddCorrelationId(HttpContext context, string correlationId)
+    {
+        context.Response.OnStarting(() =>
+        {
+            if (!context.Response.Headers.ContainsKey(_correlationIdHeader))
+            {
+                context.Response.Headers.Add(_correlationIdHeader, new[] { correlationId });
+            }          
+            return Task.CompletedTask;
+        });
+    }
+
+    #endregion
+}
